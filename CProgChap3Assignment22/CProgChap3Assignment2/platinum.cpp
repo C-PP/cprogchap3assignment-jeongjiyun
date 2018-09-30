973800541539e25b5cc4484b6bf2628265fdc2aa
jeongjiyun <41770194+jeongjiyun@users.noreply.github.com>
2018-09-30 15:30:43 +0900

    수학과 201611143 정지윤
    수학과 201611143 정지윤입니다.

--- CProgChap3Assignment22/CProgChap3Assignment2/platinum.cpp
+++ CProgChap3Assignment22/CProgChap3Assignment2/platinum.cpp
@@ -1,34 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-void function(void);
+void function(float weight);
 
 /* ANSI/ISO C function prototyping */
-int main(void) 
+int main(void)
 {
 	float weight;    /* user weight             */
 
-	printf("Are you worth your weight in platinum?\n");
+	printf("How many water molecules are there?\n");
 	printf("Let's check it out.\n");
-	printf("Please enter your weight in pounds: ");/*get input from the user  */
+	printf("Please enter weight of the water in quarts: ");/*get input from the user  */
 	scanf("%f", &weight);      /* assume platinum is $1700 per ounce          */
-							   /* 14.5833 converts pounds  */
-	printf("I will summon the butler function.\n");
 	function(weight);
-	printf("Yes. Bring me some tea and writeable DVDs.\n");
 	getchar();
 	return 0;
 }
 
 void function(float weight)       /* start of function definition */
-	{
-		float value;     /* platinum equivalent     */
-
-		value = 1700.0 * weight * 14.5833;
-		printf("weight = %f\n", weight);
-		//여기에 코딩하는 것이 필요함
-		printf("Your weight in platinum is worth $%.2f.\n", value); // 소숫점 두자리 출력
-		printf("You are easily worth that! If platinum prices drop,\n");
-		printf("eat more to maintain your value.\n");
-		system("pause");
-		return 0;
-	}
+{
+	float value;     /* platinum equivalent     */
+	value = (weight * 950) / (3.0e-23);
+	printf("weight = %f\n", weight);
+	printf("The number of water molecules is $%.2f.\n", value); // 소숫점 두자리 출력
+	system("pause");
+}
